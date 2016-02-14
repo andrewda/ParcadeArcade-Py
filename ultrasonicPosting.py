c66fdbbe1075f1e4aea927ca7e5cfec9326a2369
Evan <evanhoppermoore@gmail.com>
2016-02-14 09:39:34 -0800

    Sorry my bad
    forgot to fix error

--- ultrasonicPosting.py
+++ ultrasonicPosting.py
@@ -21,8 +21,8 @@ while (true):
         button2 = 1
 
     #if the value isnt already one, set it to one when the corresponding button's pressed
-    if (json.loads(urllib2.urlopen("http://andrew.local:1337/pull/?id=" + ).read())["response"]["sensors"]["button1"]["value"] == 0):
+    if (json.loads(urllib2.urlopen("http://andrew.local:1337/pull/?id=").read())["response"]["sensors"]["button1"]["value"] == 0):
         requests.post("http://andrew.local:1337/push/?id=" + piID + "&name=button1&value=" + button1)
-    if (json.loads(urllib2.urlopen("http://andrew.local:1337/pull/?id=" + ).read())["response"]["sensors"]["button2"]["value"] == 0):
+    if (json.loads(urllib2.urlopen("http://andrew.local:1337/pull/?id=").read())["response"]["sensors"]["button2"]["value"] == 0):
         requests.post("http://andrew.local:1337/push/?id=" + piID + "&name=button2&value=" + button2)
     time.sleep(.1)
