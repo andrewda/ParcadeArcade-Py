30667d26a073d81f9cdcc68aa9db7d42af3a6aad
bussell <joe.bussell@gmail.com>
2016-02-13 19:02:10 -0800

    got server if working

--- PostToUrl.py
+++ PostToUrl.py
@@ -7,36 +7,47 @@ Created on Fri Feb 12 23:26:00 2016
 
 import requests
 import json
-from enum import Enum
-import urllib2
+import grovepi
+import lcd
 
-class IoTypes(Enum):
-    DigitalIn = 1
-    DigitalOut = 2
-    AnalogIn = 3
-    AnalogOut = 4
-    I2C = 5
-    RPISER = 6
-    SERIAL = 7
+#from enum import Enum
+#
+#class IoTypes(Enum):
+#    DigitalIn = 1
+#    DigitalOut = 2
+#    AnalogIn = 3
+#    AnalogOut = 4
+#    I2C = 5
+#    RPISER = 6
+#    SERIAL = 7
 
+greenLed = 2
+grovepi.pinMode( greenLed, "OUTPUT" )
+grovepi.digitalWrite( greenLed, 1 )
+
+lcd.setRGB( 128, 0, 0 ) # red
 
 class Capability(dict):
     id = 0
+    moteId = 0
     name = "button"
     port = 0
-    ioType = IoTypes.AnalogIn
-    def __init__(self, id, name, port, ioType):
+    ioType = 1
+    def __init__(self, name, port, ioType):
         self.name = name 
         self.port = port
         self.ioType = ioType
+        
+    def toDict(self):
+        x = { "name" : self.name, "port" : self.port, "ioType" : self.ioType, "moteId" : self.moteId}
+        return x
     
 class Mote:
     id = 0
     name = "Simon"
     description = "Simon Game in Main Hall"
     capabilities = []
-    def __init__(self, id, name, description,capabilities=[]):
-        self.id = id
+    def __init__(self, name, description,capabilities=[]):
         self.name = name
         self.description = description
     
@@ -48,9 +59,12 @@ class Mote:
 #        return json.dumps(self, default=lambda o: o.__dict__, 
 #            sort_keys=True, indent=4)
     
-    #def toDict(self):
-
-    
+    def toDict(self):
+        x = { "name": self.name, "description" : self.description }
+        #clist = [ ob.toDict() for ob in myMote.capabilities ]
+        #x["capabilities"] = clist
+        return x
+        
     def to_JSON(self):        
         myStr = '"name": "' + self.name +'", '
         myStr += '"description": "' + self.description +'", ' 
@@ -84,25 +98,79 @@ def as_enum(d):
         return d
             
 myMote = Mote( 0, "Simon", "My new Simon" )
-myMote.addCapability( Capability( 0, "Button 1", 2, 1 ) )
-myMote.addCapability( Capability( 1, "Button 2", 3, 1 ) )
+#myMote.addCapability( Capability( "Button 1", 2, 1 ) )
+#myMote.addCapability( Capability( "Button 2", 3, 1 ) )
+myMote.addCapability( Capability( "Green LED", 2, 2 ) )
 
 #json_string = json.dumps([ob.__dict__ for ob in myMote.capabilities], cls=EnumEncoder)
 #print json_string
 
-myJson =  myMote.to_JSON()
-print "<snip>"
-print myJson
-print "</snip>"
+#myJson =  myMote.to_JSON()
+#print "<snip>"
+#print myMote.toDict()
+#print "</snip>"
 # post = json.dumps(myMote)
 
 # myJson = {     "description": "My new Simon",      "id": 0,      "name": "Simon" ,"capabilities": '[{"ioType": 1, "name": "Button 1", "port": 2}, {"ioType": 1, "name": "Button 2", "port": 3}]' }
-foo = requests.post('http://192.168.0.101:1337/add_listener', data=myJson )
-print(foo.text)
+url = 'http://192.168.0.101:1337/add_listener'
+header = {'content-type': 'application/json'}
+foo = requests.post(url, params=myMote.toDict(), headers=header)
+rslt = json.loads( foo.text)
+id = rslt["response"]["id"]
+myMote.id = id
+
+for ob in myMote.capabilities:
+    ob.moteId = id
+    
+
+grovepi.digitalWrite( greenLed, 0 )
+
+addCapUrl = 'http://192.168.0.101:1337/add_capability'
+clist = [ requests.post(addCapUrl, params=ob.toDict(), headers=header) for ob in myMote.capabilities ]
+
+#foo = requests.post('http://192.168.0.101:1337/add_listener', params=myJson, headers )
+print(myMote.id)
+
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route("/", methods=['POST'])
+def receive_json():
+    content = request.get_json()
+    if content is None:
+        return "Unable to retrieve JSON\n"
+    print(content)
+    return "Success\n"
+
+@app.route("/set", methods=['POST'])
+def respond():
+#    content = request.get_json()
+#    print('foo: '+ str(content))
+#    print('fum ' + str(request))
+#    print('fie ' + str(request.data))
+#    print('fiy ' + str(request.args))
+#    print('bye ' + str(request.values))
+
+    port = request.args["port"]
+    value = request.args["value"]
+    ioType = request.args["ioType"]
+    
+    print "port: " + port
+    print "value: " + value
+    print "ioType: " + ioType
+    
+    grovepi.digitalWrite( int(port), int(value) )
+    
+#    for ob in myMote.capabilities:
+#        print ob.name + " " + ob.port + " " + ob.value
+        
+#        if int(port) == ob.port and int(ioType) == ob.ioType:     
+#            grovepi.digitalWrite( ob.port, ob.value )
+#            print "setting port: " + port + "  ioType: " + ioType
+#        else:
+#            print "port: " + port + "  ioType: " + ioType
+    
+    return "Success\n"
 
 
-#req = urllib2.Request('http://192.168.0.101:1337/add_listener?params='  )
-#req.add_header('Content-Type', 'application/json')
-#
-#response = urllib2.urlopen(req, myJson)
-#print response
+app.run(host = '0.0.0.0')
